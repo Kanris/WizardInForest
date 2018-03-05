f457d3e920c7144540accd9e2a1339598efab174
Alexey Glukhov <kanris2@gmail.com>
2018-03-05 10:34:17 +0200

    Add comments in Attack.cs

--- Assets/Scripts/Player/Attack/Attack.cs
+++ Assets/Scripts/Player/Attack/Attack.cs
@@ -5,22 +5,20 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour {
 
-    private TextMesh[] tmCooldown;
-    private bool isInCooldown = false;
-    private double timerValue = 0d;
+    private TextMesh[] tmCooldown; //cooldown textmeshes
+    private bool isInCooldown = false; //is player need to wait, before throw another fireball
+    private double timerValue = 0d; //cooldown value
 
     private void Start()
     {
-        var textMeshGM = GameObject.FindGameObjectsWithTag("Cooldown");
+        var textMeshGM = GameObject.FindGameObjectsWithTag("Cooldown"); //get all objects with cooldown tag
 
-        tmCooldown = new TextMesh[textMeshGM.Length];
+        tmCooldown = new TextMesh[textMeshGM.Length]; //create textmesh array
 
         for (int index = 0; index < textMeshGM.Length; index++)
         {
-            tmCooldown[index] = textMeshGM[index].GetComponent<TextMesh>();
+            tmCooldown[index] = textMeshGM[index].GetComponent<TextMesh>(); //assign textmesh array with cooldown textmesshes
         }
-
-        Debug.Log(textMeshGM.Length);
     }
 
     // Update is called once per frame
@@ -31,29 +29,30 @@ public class Attack : MonoBehaviour {
             if (!FindObjectOfType<DialogueManager>().isDialogueInProcess) //if player is not in dialogue
             {
                 Instantiate(Resources.Load<GameObject>("Prefab/Fireball")); //create fireball
-                timerValue = FindObjectOfType<PlayerStats>().AttackCooldown;
-                isInCooldown = true;
+                timerValue = FindObjectOfType<PlayerStats>().AttackCooldown; //get player attack cooldown
+                isInCooldown = true; //player can't create another fireball
 
-                StartCoroutine(StartCooldown());
+                StartCoroutine(StartCooldown()); //start countdown
             }
         }
 	}
 
+    //display cooldown to the player
     private IEnumerator StartCooldown()
     {
-        while (isInCooldown)
+        while (isInCooldown) //while fireball is in cooldown
         {
             timerValue = Math.Round(timerValue, 1);
-            if (timerValue == 0d)
+            if (timerValue == 0d) //if time is up
             {
-                isInCooldown = false;
-                tmCooldown[0].text = tmCooldown[1].text = string.Empty;
+                isInCooldown = false; //player can throw another fireball
+                tmCooldown[0].text = tmCooldown[1].text = string.Empty; //empty cooldown textmeshes
             }
             else
             {
-                timerValue -= 0.1d;
-                tmCooldown[0].text = tmCooldown[1].text = timerValue.ToString() + "s";
-                yield return new WaitForSeconds(0.1f);
+                timerValue -= 0.1d; //another 0.1s 
+                tmCooldown[0].text = tmCooldown[1].text = timerValue.ToString() + "s"; //show cooldown value
+                yield return new WaitForSeconds(0.1f); //wait for 0.1s
             }
         }
     }
