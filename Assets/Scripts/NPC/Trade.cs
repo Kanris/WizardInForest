601c9d6bc4e39be575cac5bc9fd517c53183c782
Alexey Glukhov <kanris2@gmail.com>
2018-03-08 12:02:45 +0200

    Add big shop. Trader now answers only when player is near

--- Assets/Scripts/NPC/Trade.cs
+++ Assets/Scripts/NPC/Trade.cs
@@ -1,54 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Trade : MonoBehaviour {
-
-    [SerializeField]
-    private TextMesh[] traderAnswer;
 
     [SerializeField]
     [TextArea(2, 10)]
     private string sentence;
 
-    private void Update()
+    private TextMesh[] traderAnswer;
+
+    private bool isPlayerNear = false;
+
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.E) && !PauseMenu.isGamePaused)
+        InitializeAnswerTextMeshes();
+    }
+
+    private void InitializeAnswerTextMeshes()
+    {
+        traderAnswer = new TextMesh[gameObject.transform.childCount];
+
+        for (int index = 0; index < traderAnswer.Length; index++)
         {
-            AnswerToPlayer(this.sentence);
+            traderAnswer[index] = gameObject.transform.GetChild(index).GetComponent<TextMesh>();
         }
     }
 
-    private void AnswerToPlayer(string sentence)
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerNear && !PauseMenu.isGamePaused)
+        {
+            DisplaySentence(sentence); //displaye trader message
+        }
+    }
+
+    private void DisplaySentence(string sentence)
     {
         traderAnswer[0].text = traderAnswer[1].text = sentence; //displaye trader message
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
-    { 
-        ShowInteractionButton(true, collision); //show interaction button
+    {
+        InteractionWithPlayer(true, collision);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        ShowInteractionButton(false, collision); //hide all interactions with trader
+        InteractionWithPlayer(false, collision);
     }
 
-    private void ShowInteractionButton(bool show, Collider2D collision)
+    private void InteractionWithPlayer(bool show, Collider2D collision)
     {
         if (collision.CompareTag("Player") && collision.isTrigger) //if player is near trader
         {
+            isPlayerNear = show;
             var hudInteractionButton = FindObjectOfType<PlayerInteractionButtons>();
 
             if (show)
             {
-                hudInteractionButton.ShowInteractionButton(PlayerInteractionButtons.Buttons.E); //show button e
+                hudInteractionButton.ShowInteractionButton(PlayerInteractionButtons.Buttons.E);
             }
             else
             {
                 hudInteractionButton.HideInteractionButton(); //hide interaction button
-                AnswerToPlayer(string.Empty); //clear message dialoge
+                DisplaySentence(string.Empty);
             }
         }
     }
