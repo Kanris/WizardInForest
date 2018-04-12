ebaecd009e554967f3dcba8335f491e5dc006382
Alexey Glukhov <kanris2@gmail.com>
2018-04-12 10:39:45 +0300

    Add script for buying item

---
+++ Assets/BuyItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyItem : MonoBehaviour {
+
+    [SerializeField]
+    private int Price = 0;
+
+    [SerializeField]
+    private string Item = "Empty";
+
+    public void Buy()
+    {
+        FindObjectOfType<Inventory>().SpendCoins(Price, Item);
+    }
+}
